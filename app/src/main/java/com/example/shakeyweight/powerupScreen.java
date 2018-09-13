310a8ac841054b4c7924d0fb0144bb02e595cf24
Dean Campagnolo <dcampagn@ucsc.edu>
2018-09-13 15:34:26 -0700

    Added powerupScreen

---
+++ app/src/main/java/com/example/shakeyweight/powerupScreen.java
@@ -0,0 +1,13 @@
+package com.example.shakeyweight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class powerupScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_powerup_screen);
+    }
+}
