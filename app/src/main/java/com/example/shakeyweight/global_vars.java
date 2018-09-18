888c4dce514007c67cd6dd88b77f392bb5d723b8
Dean Campagnolo <dcampagn@ucsc.edu>
2018-09-18 14:14:50 -0700

    forgot to commit this in the last commit

--- app/src/main/java/com/example/shakeyweight/global_vars.java
+++ app/src/main/java/com/example/shakeyweight/global_vars.java
@@ -3,16 +3,57 @@ import android.app.Application;
 import android.content.Context;
 
 public class global_vars extends Application{
-    int shakes;
-    double rate;
+    static int shakes;
+    static int rate;
+    static int amountOfShakerMod;
     private static Application sApplication;
 
     public static Application getApplication() {
         return sApplication;
     }
+
+
     public static Context getContext() {
         return getApplication().getApplicationContext();
     }
 
+    public int getShakes(){
+        return shakes;
+    }
+
+    public void setShakes(int amount){
+        shakes = amount;
+    }
+
+    public int getRate(){
+        return rate;
+    }
+
+    public void addToShakes(int amount){
+        shakes += amount;
+    }
+
+    public void subtractFromShakes(int amount){
+        shakes -= amount;
+    }
+
+    public void setRate(int amount){
+        rate = amount;
+    }
+
+    public void addToRate(int amount){
+        rate += amount;
+    }
+
+    public int getAmountOfShakerMod() {
+        return amountOfShakerMod;
+    }
+
+    public void setAmountOfShakerMod(int amountOfShakerMod) {
+        global_vars.amountOfShakerMod = amountOfShakerMod;
+    }
+    public void addToAmountOfShakerMod(int amount){
+        amountOfShakerMod+=amount;
+    }
 
 }
